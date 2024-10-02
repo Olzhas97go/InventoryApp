dcefacc1d9a5da681b8fd603e808bc7c977c76d8
Olzhas Sagidolla <olzhas_sagidolla@epam.com>
2024-10-02 15:47:31 +0500

    Initial commit

---
+++ InventoryStore/Data/InventoryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication9.Models;
+
+namespace WebApplication9.Data;
+public class InventoryContext : DbContext
+{
+    public InventoryContext(DbContextOptions<InventoryContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
